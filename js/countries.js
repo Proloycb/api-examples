7f7409dbc2a18e94bd8efdfd54180a67fc7d28f5
Proloycb <proloychacroborty@gmail.com>
2022-02-23 18:24:05 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,36 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.classList.add('country');
+        div.innerHTML = `
+            <h3>${country.name.common}</h3>
+            <p>${country.capital}</p>
+            <button onclick ="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+        countriesDiv.appendChild(div);
+    });
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+}
+
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h5>${country.name.common}</h5>
+        <p>${country.population}</p>
+        <img src ="${country.flags.png}">
+    `
+}
---
+++ js/kanye.js
@@ -0,0 +1,11 @@
+function loadQuotes() {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuotes(data));
+};
+
+const displayQuotes = quote => {
+    console.log(quote);
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerText = quote.quote;
+}
